4a6ee1c3c47465431d7256d572b2dceda56360b9
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2021-07-31 23:52:48 +0530

    Added comments for brute force solution of max sub array sum problem

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # This is DSA practice problems and their solutions in python3 
 
-Here is a list of problmes that are included in this repo
+Here is a list of problems that are included in this repo
 
 | Problem Name | Problem Brief Desc | Link |
 |--------------|--------------------|------|
+| Max Sub Array Sum | Find the max sum of contagious sub array in a given array | [max-sub-array-prob](./max-subarray-prob.py)
--- max-subarray-prob.py
+++ max-subarray-prob.py
@@ -1,28 +1,34 @@
-# Problem - Given an array find the contigous sub array that has the maximum sum
+# ---------------------- Problem -------------------------------------------------- 
+# Given an array find the contigous sub array that has the maximum sum
 # Example - Given the following array 
 #		[-2,-1,2,5,-3,4]
 # 	    The sub array that has the maximum sum is [2,5,-3,4]
 # 	    Also the maximum sum is 8
+# ----------------------------------------------------------------------------------
 
 
+# Brute force solution -------------------------------------------------------------
+# In this appraoch we find all the possible sub arrays in the given array
+# Method to find all possible sub arrays is to iterate through each item
+# Then keep adding rest of the items to the item one by one in a loop
 
-# Brute force solution of the problem 
-# In this appraoch we find all the possible sub arrays in the 
+# Time Complexity O(n^2) - this is beacuse of the two nexted loops
+# Space Complexity O(1) - Constant as we need to only store a few things and its not a ever increasing list
 
 def max_subarray_sum_brute(arr):
     global_max = float('-inf')
     result_arr = []
     l = len(arr)
     
-    for i in range(0,l-1):
+    for i in range(0,l-1):                               # loop through each item in the array
         current_max = float('-inf')
         
-        for j in range(i,l-1):
-            if i==j:
+        for j in range(i,l-1):                           # for each item then add other items in the array to create a subarray
+            if i==j:                                     # this is done to avoid summing the same item with itself
                 current_max = arr[i]
             else:
-                current_max += arr[j]
-            global_max = max(current_max, global_max)
+                current_max += arr[j]                    # sum of the sub array by adding the sum of last sub array to create the new sum
+            global_max = max(current_max, global_max)    # compare the current sum with the global max , which ever is greater take that
     
     return global_max, result_arr
 
@@ -30,8 +36,13 @@ def max_subarray_sum_brute(arr):
 
 
 
-# driver code
+def max_subarray_sum(arr):
+    pass
 
+
+
+
+# driver code
 def main():
     arry = [-2,-1,2,5,-3,4]
     result, sub_arr = max_subarray_sum_brute(arry)
