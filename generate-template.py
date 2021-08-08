3a2ff9a1f737d98f2286be7457bfe84e4b88ff34
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2021-08-08 11:48:31 +0530

    Added templating code to generate solution files easily

---
+++ generate-template.py
@@ -0,0 +1,58 @@
+import jinja2
+from jinja2 import Template
+
+
+data = '''
+{% raw %}
+# ------------------------------- Problem Description ----------------------------
+#
+#
+# ---------------------------------------------------------------------------------
+
+
+
+# ------------------------------ Brute Force Solution ------------------------------
+#
+#
+#
+# Time Complexity :
+#
+# Space Complexity
+# ----------------------------------------------------------------------------------
+
+
+def function_name_slow():
+    pass
+
+
+
+# ----------------------------- Effiecient Solution --------------------------------
+#
+#
+#
+# Time Complexity:
+#
+# Space Complexity:
+#------------------------------------------------------------------------------------
+
+def function_name_efficient():
+    pass
+
+
+
+# Driver Code
+
+def main():
+    pass
+
+
+
+main()
+{% endraw %}
+'''
+
+tm = Template(data)
+
+with open("file.py", "w") as f:
+    msg = tm.render()
+    f.write(msg)
