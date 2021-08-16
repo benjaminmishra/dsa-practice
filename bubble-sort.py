0037b037b28409981cb17ca07b36411059fc7b7d
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2021-08-16 12:58:10 +0530

    New problem - buy selll stock once

---
+++ bubble-sort.py
@@ -0,0 +1,64 @@
+
+
+# ------------------------------- Problem Description ----------------------------
+# Write the Bubble Sort Alorithm
+#
+# ---------------------------------------------------------------------------------
+
+
+
+
+# -----------------------------  Solution --------------------------------
+#
+#
+#
+# Time Complexity:
+#
+# Space Complexity:
+#------------------------------------------------------------------------------------
+
+def bubble_sort(arr : list[int]) -> list[int]:
+    n : int = len(arr)
+    j : int = 0
+    
+    # we need to run the comparision for all positions in the array
+    for j in range(n):
+        # for each item in the array
+        # check if that item is greater than the next item
+        # if yes then swap otherwise move on to next
+
+        # do this comparision only upto the n-1-j because the last item is already sorted
+        for i in range(n-1-j):
+            if arr[i] > arr[i+1]:
+                # swap
+                temp = arr[i+1]
+                arr[i+1] = arr[i]
+                arr[i] = temp
+
+    return arr
+
+    
+
+
+
+# Driver Code
+
+def main():
+    # Test Case 1 - Medium case , some elements are in order and other are not
+    input_arr_1 = [12,11,13,5,6,7]
+    sorted_arry_1 = bubble_sort(input_arr_1)
+    print(sorted_arry_1)
+
+    # Test Case 2 - Worst case , entire array is inverted or in descending order
+    input_arr_2 = [10,8,7,6,5,4,3,2,1]
+    sorted_arry_2 = bubble_sort(input_arr_2)
+    print(sorted_arry_2)
+
+    # Test Case 3 - Best case , the array is already sorted
+    input_arr_3 = [20,30,40,50,60,100]
+    sorted_arry_3 = bubble_sort(input_arr_3)
+    print(sorted_arry_3)
+
+
+
+main()
--- buy-sell-stock.py
+++ buy-sell-stock.py
@@ -14,8 +14,8 @@
 #                           to keep track of buy sell and max profit at any given time.
 # ----------------------------------------------------------------------------------
 
-
 def find_max_profit_slow(prices : list[int])->int:
+    # variables to keep track of some things, optional except the max profit
     buy : int = 0
     sell :int = 0
     max_profit: int = 0
@@ -40,26 +40,49 @@ def find_max_profit_slow(prices : list[int])->int:
 
 
 
-# ----------------------------- Effiecient Solution --------------------------------
+# ----------------------------- Efficient Solution  O(n)--------------------------------
+# variation of Kaden's algo. All we need to do here is go to each price in the given array, 
+# and for each position see what is the least price seen so far (i.e. till that postion of the array).
+# Once we know that what is the minimum seen so far, we find the difference of the current price with the 
+# minumum seen so far. This becomes the current maximum profit. Then we compare this current maximum profit
+# with the global maximum so far. Which ever is greater, we take that. At the end of the array the 
+# max profit value becomes the max profit that can be made.
 #
+# Time Complexity: O(n) , this only requires a single pass of the array
 #
-#
-# Time Complexity:
-#
-# Space Complexity:
+# Space Complexity: O(1) , constant since we barely store anything extra.
 #------------------------------------------------------------------------------------
 
-def function_name_efficient():
-    pass
+def find_max_profit_efficient(prices : list[int])->int:
+    max_profit = float("-inf")
+    min_price_so_far : int = prices[0]
+
+    for i in range(1,len(prices)):
+        min_price_so_far= min(min_price_so_far, prices[i])
+        profit = prices[i] - min_price_so_far
+        max_profit = max(profit, max_profit)
+    
+    return max_profit
 
 
 
 # Driver Code
 
 def main():
-    input_1 : list[int] = [310,315,275,295,260,270,290,230,255,250,800,500,345,890,455, 555,500,600,675,680,690]
+    input_1 : list[int] = [310,315,275,295,260,270,290,230,255,250,800,500,345,890,455,555,500,600,675,680,690]
     result_1 : int = find_max_profit_slow(input_1)
-    print(result_1)
+    result_2 : int = find_max_profit_efficient(input_1)
+    
+    print("Slow method :" + str(result_1))
+    print("Fast Method :" + str(result_2))
 
+    input_2 : list[int] = [310,315,275,295,260,270,290,230,255,250]
+    result_3 : int = find_max_profit_slow(input_2)
+    result_4 : int = find_max_profit_efficient(input_2)
+    
+    print("Slow method :" + str(result_3))
+    print("Fast Method :" + str(result_4))
+
+    
 
 main()
