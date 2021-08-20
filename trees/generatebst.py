3b5dd9b74a63eea291cbf437ec7b51805bfdebcc
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2021-08-20 23:45:17 +0530

    Updated BST Operations

--- trees/generatebst.py
+++ trees/generatebst.py
@@ -1,11 +1,26 @@
-import BinarySearchTree
+from .BinarySearchTree import BinarySearchTree
 
 def create_bst():
     bst = BinarySearchTree(30)
     bst.left_child = BinarySearchTree(7)
-    bst.right_child = BinarySearchTree(80)
+    bst.right_child = BinarySearchTree(70)
     bst.left_child.left_child = BinarySearchTree(6)
     bst.left_child.right_child = BinarySearchTree(10)
     bst.right_child.left_child = BinarySearchTree(50)
     bst.right_child.right_child = BinarySearchTree(85)
+    bst.right_child.right_child.right_child = BinarySearchTree(90)
     return bst
+
+
+
+
+#######################
+
+#           30
+#          /   \
+#        7      70
+#       / \     / \
+#      6  10   50  85
+#                   \
+#                    90
+########################
--- valid-bst.py
+++ valid-bst.py
@@ -1,4 +1,5 @@
-from trees import BinarySearchTree, generatebst
+from trees import BinarySearchTree
+from trees import generatebst
 
 
 # ------------------------------- Problem Description ----------------------------
@@ -19,30 +20,31 @@ from trees import BinarySearchTree, generatebst
 
 
 def validate_bst(root : BinarySearchTree)-> bool:
-    
+    return is_valid(root, float("inf"), float("-inf"))
 
 
+def is_valid(node : BinarySearchTree, max, min) -> bool:
+    if not node:
+        return True
 
-# ----------------------------- Effiecient Solution --------------------------------
-#
-#
-#
-# Time Complexity:
-#
-# Space Complexity:
-#------------------------------------------------------------------------------------
+    if node.data < min or node.data > max:
+        return False    
 
-def function_name_efficient():
-    pass
+    return is_valid(node.left_child, node.data, min) and is_valid(node.right_child, max, node.data)
 
 
 
 # Driver Code
 
 def main():
-    root : BinarySearchTree = generatebst.create_bst()
-    result = validate_bst(root)
-    print(result)
+    root_1 : BinarySearchTree = generatebst.create_bst()
+    result_1 = validate_bst(root_1)
+    print(result_1)
+
+    root_2 : BinarySearchTree = None
+    result_2 = validate_bst(root_2)
+    print(result_2)
+
 
 
 
